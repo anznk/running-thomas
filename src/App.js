faa722887d66ac8c0f49b261f3c030ddddb729c1
Sander Demeester <demeester.sander@gmail.com>
2020-10-12 21:02:39 -0700

    Create a simple Canvas function-element

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
 //import './App.css';
-import Test from './Test.js'
 import Canvas from './visualsObjects/canvas.js'
 
 function App() {
@@ -11,9 +10,7 @@ function App() {
       <header className="App-header">
         <p>
         <Canvas />
-        <Test a="Hello World!"/>
         </p>
-
       </header>
     </div>
   );
