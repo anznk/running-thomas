09aecd02049374f079c2a9727c484ada882732b7
Sander Demeester <demeester.sander@gmail.com>
2020-10-27 19:58:23 -0700

    Use React context to share share between partent component and child component

--- src/GameProvider.jsx
+++ src/GameProvider.jsx
@@ -1,18 +1,23 @@
 import React, { useRef, useState, useEffect, useReducer} from "react";
 import GameObject from "./components/GameObject.jsx";
-import Canvas from './components/GameObject.jsx'
 
 const LocationContext = React.createContext();
 
 const GameProvider = (props) => {　
   const [x, setX] = useState(200)
   
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setX(x => (x + 1))
+    }, 1000 / 60);
+ 
+    return function(){
+      clearInterval(interval)
+    }
+  }, []);
+  
   return (
-    <LocationContext.Provider
-      value={
-        x
-      }
-      >
+    <LocationContext.Provider value={x}>
         <GameObject />
       </LocationContext.Provider>
   )
--- src/components/GameObject.jsx
+++ src/components/GameObject.jsx
@@ -4,9 +4,8 @@ import {LocationContext} from '../GameProvider.jsx'
 // <name of function> <arguments> { }
 const GameObject = props => {
   const canvasRef = useRef(null)
-  const bigX = useContext(LocationContext)
+  const x = useContext(LocationContext)
 
-  const [x, setX] = useState(200)
   const [y, setY] = useState(220)
 
   const draw = (ctx, x, y) => {
@@ -14,15 +13,12 @@ const GameObject = props => {
     ctx.fillStyle = '#D2691E';
     ctx.beginPath();
     //ctx.arc(frameCount, 600, 20*Math.sin(frameCount*0.05)**2, 0, 2*Math.PI)
-    ctx.arc(bigX,y, 20*Math.sin(10*0.05)**2, 0, 2*Math.PI)
+    ctx.arc(x,y, 20*Math.sin(10*0.05)**2, 0, 2*Math.PI)
     ctx.fill();
   }
   
   // useEffect is executed when 'render' is called on the Canvas element
   useEffect(() => {
-    const interval = setInterval(() => {
-      setX(x => parseInt(x + 1) % window.innerHeight)
-    },7)
 
     const canvas = canvasRef.current
     const context = canvas.getContext('2d')
@@ -32,10 +28,7 @@ const GameObject = props => {
     
     //Our draw came here
     draw(context, parseInt(x % canvas.width), parseInt(y % canvas.height))
-    
-    return () => {
-      clearInterval(interval)
-    }
+
   }, [draw])
   
   return (
