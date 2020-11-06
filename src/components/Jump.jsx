074c026e7b7886e7a24958d8e0c5c9876fa462e1
Anzu <anzunakayama@gmail.com>
2020-11-05 16:09:29 -0800

    add Thomas image

--- src/components/Jump.jsx
+++ src/components/Jump.jsx
@@ -1,16 +1,18 @@
 import React, {useRef, useEffect} from 'react';
+import Thomas from '../images/Thomas.jpg'
+
+
 
 let flgUp = false;
 let flgJump = false;
 const Jump = props => {
   window.addEventListener('keyup', e => { flgUp = true });
-
   const canvasRef = useRef(null);
   let rectangle = {
-    height:5,
+    height:30,
     jumping:false,
-    width:32,
-    x:80, // center of the canvas
+    width:90,
+    x:50, // center of the canvas
     x_velocity:0,
     y:0,
     y_velocity:0
@@ -18,9 +20,11 @@ const Jump = props => {
     const loop = () => {
       const canvas = canvasRef.current;
       const context = canvas.getContext('2d');
-      context.canvas.height = 140;
+      context.canvas.height = 200;
       context.canvas.width = 1200; 
-
+      const img = new Image();
+      img.src = Thomas;
+      
       if (flgUp && !flgJump) {
         rectangle.y_velocity -= 15;
         flgJump = true;
@@ -33,17 +37,17 @@ const Jump = props => {
       rectangle.x_velocity *= 0.9;// friction
       rectangle.y_velocity *= 1.1;// friction
 
-      if (rectangle.y > 130) {
+      if (rectangle.y > 160) {
         flgJump = false;
-        rectangle.y = 130;
+        rectangle.y = 160;
         rectangle.y_velocity = 0;
       }
 
       context.fillStyle = "white";
-      context.fillRect(0, 0, 180, 300);// x, y, width, height
+      context.fillRect(0, 0, 180, 100);// x, y, width, height
       context.fillStyle = "#ff0000";// hex for red
       context.beginPath();
-      context.rect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
+      context.drawImage(img, rectangle.x, rectangle.y, rectangle.width, rectangle.height);
       context.fill();
       context.beginPath();
       context.stroke();
@@ -54,6 +58,7 @@ const Jump = props => {
 
   useEffect(() => {
     window.requestAnimationFrame(loop);
+
   }, [])
 
   return(
