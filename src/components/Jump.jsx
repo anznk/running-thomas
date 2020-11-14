df7b659a71c37b886b44b2c76c73b6385535a037
Anzu <anzunakayama@gmail.com>
2020-11-13 21:01:02 -0800

    modified jump.jsx

--- src/components/Jump.jsx
+++ src/components/Jump.jsx
@@ -1,22 +1,34 @@
-import React, {useRef, useEffect} from 'react';
+import React, {useContext, useRef, useEffect} from 'react';
 import Thomas from '../images/Thomas.jpg'
-
-
+import {GameObjects} from '../GameProvider.jsx'
 
 let flgUp = false;
 let flgJump = false;
+
 const Jump = props => {
-  window.addEventListener('keyup', e => { flgUp = true });
-  const canvasRef = useRef(null);
-  let rectangle = {
-    height:30,
-    jumping:false,
-    width:90,
-    x:50, // center of the canvas
-    x_velocity:0,
-    y:0,
-    y_velocity:0
+  let RunnerRoadCanvasRef = useRef(null);
+  const gameObjects = useContext(GameObjects);
+
+  const draw = (ctx) => {
+    for(let i = 0; i < gameObjects.objects.length; i++){
+        let x = gameObjects.objects[i].coordinates['x'];
+        let y = gameObjects.objects[i].coordinates['y'];
+        
+        console.log('x: ' + x, 'y: '+ y);
+        drawCircle(ctx, x,y);
+    }
   }
+    window.addEventListener('keyup', e => { flgUp = true });
+    const canvasRef = useRef(null);
+    let objectThomas = {
+      height:30,
+      jumping:false,
+      width:90,
+      x:50, // center of the canvas
+      x_velocity:0,
+      y:0,
+      y_velocity:0
+    }
     const loop = () => {
       const canvas = canvasRef.current;
       const context = canvas.getContext('2d');
@@ -24,31 +36,38 @@ const Jump = props => {
       context.canvas.width = 1200; 
       const img = new Image();
       img.src = Thomas;
-      
+      draw(context);
       if (flgUp && !flgJump) {
-        rectangle.y_velocity -= 15;
+        objectThomas.y_velocity -= 15;
         flgJump = true;
         flgUp = false;
       }
 
-      rectangle.y_velocity += 2.0;// gravity
-      rectangle.y += rectangle.y_velocity;
-      rectangle.y_velocity *= 1.1;// friction
+      objectThomas.y_velocity += 2.0;// gravity
+      objectThomas.y += objectThomas.y_velocity;
+      objectThomas.y_velocity *= 1.1;// friction
 
-      if (rectangle.y > 160) {
+      if (objectThomas.y > 160) {
         flgJump = false;
-        rectangle.y = 160;
-        rectangle.y_velocity = 0;
+        objectThomas.y = 160;
+        objectThomas.y_velocity = 0;
       }
-      context.drawImage(img, rectangle.x, rectangle.y, rectangle.width, rectangle.height);
+      context.drawImage(img, objectThomas.x, objectThomas.y, objectThomas.width, objectThomas.height);
       // call update when the browser is ready to draw again
       window.requestAnimationFrame(loop);
     };
 
   useEffect(() => {
     window.requestAnimationFrame(loop);
-
   }, [])
+
+  const drawCircle = (ctx, x,y) =>{
+    //ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    ctx.fillStyle = '#D2691E';
+    ctx.beginPath();
+    ctx.arc(x,y, 20*Math.sin(10*0.05)**2, 0, 2*Math.PI);
+    ctx.fill();
+  }
 
   return(
     <>
